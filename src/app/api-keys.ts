7d87c69613dd0ae1b914d7447fe83ccead76c565
Larry <larryjamestaylor@gmail.com>
2017-08-25 11:01:50 -0700

    create project in firebase and add to api-keys

---
+++ src/app/api-keys.ts
@@ -0,0 +1,7 @@
+export var masterFirebaseConfig = {
+  apiKey: "AIzaSyB57FJBnjRu1TYiCZYGJsNjlRIAMB3QAt4",
+      authDomain: "theatre-group.firebaseapp.com",
+      databaseURL: "https://theatre-group.firebaseio.com",
+      storageBucket: "theatre-group.appspot.com",
+      messagingSenderId: "153431791980"
+};
